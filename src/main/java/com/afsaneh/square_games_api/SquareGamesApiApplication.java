7525d63ae2de57b187aa9bde3296240d17bbcc55
Afsaneh Gheisariyeh <a.gheisariyeh25@gmail.com>
2026-09-10 11:12:26 +0200

    Initialize Spring Boot project with Maven and Spring Web

---
+++ src/main/java/com/afsaneh/square_games_api/SquareGamesApiApplication.java
@@ -0,0 +1,13 @@
+package com.afsaneh.square_games_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquareGamesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquareGamesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afsaneh/square_games_api/SquareGamesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afsaneh.square_games_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareGamesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
